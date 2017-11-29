81ab04d9babae5f6c1e5091f85df790f67712f91
nickward13 <nickward@microsoft.com>
2017-11-29 11:57:10 +0800

    added Permanent=True tag

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -2,5 +2,5 @@
 
 az login
 
-az group create --name hectagon-functions --location "Australia Southeast"
+az group create --name hectagon-functions --location "Australia Southeast" --tags Permanent=True
 az group deployment create --resource-group hectagon-functions --template-file azuredeploy.json --parameters @azuredeploy.parameters.prod.json
